3b591dc9769312a58258cb18868169c61e2bf173
00limited <hantu.legi@gmail.com>
2022-09-26 15:02:53 +0700

    frontEnd Dumbflix

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+// export const API = axios.create({
+//   baseURL: "http://localhost:5000/api/v1/",
+// });
+// Create base URL API
 export const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1/",
+  baseURL: process.env.REACT_APP_BASEURL, // Get REACT_APP_BASEURL from env
 });
 
 export const setAuthToken = (token) => {
